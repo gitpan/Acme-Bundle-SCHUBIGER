8a12a86171450936eb15e5b0b32cac0d78b9623b
Alberto Manuel Brandão Simões <ambs@perl-hackers.net>
2005-02-22 22:01:39 +0000

    Import of AMBS/Acme-Bundle-SCHUBIGER-0.01 from CPAN.
    gitpan-cpan-distribution: Acme-Bundle-SCHUBIGER
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         AMBS/Acme/Acme-Bundle-SCHUBIGER-0.01.tar.gz
    gitpan-cpan-author:       AMBS
    gitpan-cpan-maturity:     released

---
+++ lib/Acme/Bundle/SCHUBIGER.pm
@@ -0,0 +1,60 @@
+package Acme::Bundle::SCHUBIGER;
+
+$VERSION = '0.01';
+
+=head1 NAME
+
+Acme::Bundle::SCHUBIGER - install all modules by SCHUBIGER
+
+=head1 SYNOPSIS
+
+cpan Acme::Bundle::SCHUBIGER
+
+=head1 CONTENTS
+
+Algorithm::MedianSelect
+
+build::config
+
+Convert::ASCII::String
+
+Dir::Split
+
+Filesys::Getfsent
+
+make2build
+
+Math::Factor
+
+Math::Prime::Simple
+
+Net::Inetd
+
+Sort::NoCase
+
+String::Charcount
+
+Sub::Fallback
+
+Text::Rhombus
+
+Tie::DiskUsage
+
+Tie::Mounted
+
+WWW::Cyborg
+
+=head1 AUTHOR
+
+#perl on irc.perl.org
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2004-2005 SCHUBIGER
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=cut
+
+1; 
